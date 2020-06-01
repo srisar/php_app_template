be4b0dd8d021e2ab6f5586cfd02864570cfeeb24
M Srisaravana <saravanamuthaly@gmail.com>
2020-06-01 11:23:52 +0530

    base blocks

--- views/index.view.php
+++ views/index.view.php
@@ -14,7 +14,16 @@ use App\Core\App;
 </head>
 <body>
 
-<?= toCurrencyFormat(2) ?>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-sm-6 col-12 text-center">
+            <h1>PHP APP TEMPLATE</h1>
+            <hr>
+            <p>This is a simple scaffolding app for router base php application. Feel free to clone and work!</p>
+            <p>By Srisaravana</p>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
